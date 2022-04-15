3969fd67716a5556e1c93c608a5fc7b644f40d78
Arpita Chowdhury <achoudhary@sumologic.com>
2022-04-15 15:05:24 +0530

    update

--- baseline/heybatch.sh
+++ baseline/heybatch.sh
@@ -1,4 +1,4 @@
-url= "<apache-server-http-url like http://54.183.167.231>"
+url="<apache-server-http-url like http://54.183.167.231>"
 ports=(8081 8082 8083 8084 8085 8086 8087 8088)
 concurrent_sessions=50
 duration="6h"
